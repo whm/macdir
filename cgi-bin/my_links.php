881e3c8c457cd3c8067563c20e3f52d70bee3657
Bill MacAllister <bill@ca-zephyr.org>
2020-01-02 08:05:15 +0000

    Update the links display to show shared links.

--- cgi-bin/my_links.php
+++ cgi-bin/my_links.php
@@ -9,6 +9,101 @@ $heading = "Search My Links";
 require('inc_init.php');
 require('inc_header_my_links.php');
 
+# ----------------------------------------------------------------------
+# Subroutines
+
+// ----------------------------------------
+// Pull the UID form a DN.  If there is no uid just the whole dn
+
+function uid_from_dn($dn) {
+    $return = preg_match('/,{0,1}uid=([^,]*)/i', $dn, $mat);
+    if (empty($mat[1])) {
+        $uid = $dn;
+    } else {
+        $uid = $mat[1];
+    }
+    return $uid;
+}
+
+// ----------------------------------------
+// Display search results
+
+function display_links($edit_flag, $title, $info) {
+
+    global $CONF;
+
+    echo "<h2>$title</h2>\n";
+    echo "<table>\n";
+    echo "<thead>\n";
+    echo "<tr>\n";
+    echo " <th>Description</th>\n";
+    echo " <th>URL</th>\n";
+    echo " <th>Username</th>\n";
+    echo " <th>Password</th>\n";
+    echo "</tr>\n";
+    echo "</thead>\n";
+    echo "<tbody>\n";
+    for ($i=0; $i<$info["count"]; $i++) {
+        $a_cn = $info[$i]["cn"][0];
+        $a_cn_url = urlencode($a_cn);
+
+        $a_maint_link
+            = '<a href="my_links_maint.php' . '?in_cn=' . $a_cn_url . '">'
+            . '<img src="/macdir-images/icon-edit.png" border="0"></a>';
+        $a_desc    = empty($info[$i]["description"][0])
+            ? '' : $info[$i]["description"][0];
+
+        $a_url_list = '&nbsp;';
+        if (!empty($info[$i]["prideurl"][0])) {
+            $url_list = explode(' ', $info[$i]["prideurl"][0]);
+            $a_br = '';
+            $a_url_list = '';
+            foreach ($url_list as $u) {
+                $a_url_list .= $a_br
+                    . '<a href="' . htmlentities($u) . '" target="_BLANK">'
+                    . $u . '</a>';
+                $a_br = "<br/>\n";
+            }
+        }
+
+        $a_pw_list = '&nbsp;';
+        if (!empty($info[$i]["pridecredential"][0])) {
+            $pw_list = explode(' ', $info[$i]["pridecredential"][0]);
+            $a_br = '';
+            $a_pw_list = '';
+            foreach ($pw_list as $pw) {
+                $this_pat = '/^' . $CONF['key_prefix'] . '(.*)/';
+                if (!empty($CONF['key'])
+                    && preg_match($this_pat, $pw, $m))
+                {
+                    $this_epw = $m[1];
+                    $pw = macdir_decode($this_epw);
+                }
+                $a_pw_list .= $a_br . $pw;
+                $a_br = "<br/>\n";
+            }
+        }
+
+        if ($edit_flag) {
+            $a_desc_link = $a_maint_link . $a_desc;
+        } else {
+            $a_desc_link = $a_desc . ' (' . uid_from_dn($info[$i]['dn']) . ')';
+        }
+        echo "<tr>\n";
+        echo " <td>$a_desc_link</td>\n";
+        echo " <td>${a_url_list}</td>\n";
+        echo ' <td>' . nbsp_html($info[$i]["linkuid"][0]) . "</td>\n";
+        echo " <td>${a_pw_list}</td>\n";
+        echo "</tr>\n";
+    }
+    echo "</tbody>\n";
+    echo "</table>\n";
+    return;
+}
+
+# ----------------------------------------------------------------------
+# Main Routine
+
 $ds = macdir_bind($CONF['ldap_server'], 'GSSAPI');
 
 # create a form to attribute mapping
@@ -119,82 +214,40 @@ if ( !empty($_SESSION['in_msg']) ) {
 
 $link_base = "uid=${this_uid},${ldap_user_base}";
 $base_filter .= $class_filter;
-
+$filter = '(&(objectclass=pridelistobject)'. $base_filter. ')';
 $return_attr = array('cn',
                      'description',
                      'prideurl',
                      'linkuid',
                      'pridecredential',
                      'prideurlprivate');
-$filter = '(&(objectclass=pridelistobject)'. $base_filter. ')';
 $sr = ldap_search($ds, $link_base, $filter, $return_attr);
 ldap_sort($ds, $sr, 'description');
 $info = ldap_get_entries($ds, $sr);
 $ret_cnt = $info["count"];
 if ($ret_cnt) {
-    echo "<table>\n";
-    echo "<thead>\n";
-    echo "<tr>\n";
-    echo " <th>Description</th>\n";
-    echo " <th>URL</th>\n";
-    echo " <th>Username</th>\n";
-    echo " <th>Password</th>\n";
-    echo "</tr>\n";
-    echo "</thead>\n";
-    echo "<tbody>\n";
-    for ($i=0; $i<$info["count"]; $i++) {
-        $a_cn = $info[$i]["cn"][0];
-        $a_cn_url = urlencode($a_cn);
-
-        $a_maint_link
-            = '<a href="my_links_maint.php' . '?in_cn=' . $a_cn_url . '">'
-            . '<img src="/macdir-images/icon-edit.png" border="0"></a>';
-        $a_desc    = empty($info[$i]["description"][0])
-            ? '' : $info[$i]["description"][0];
-
-        $a_url_list = '&nbsp;';
-        if (!empty($info[$i]["prideurl"][0])) {
-            $url_list = explode(' ', $info[$i]["prideurl"][0]);
-            $a_br = '';
-            $a_url_list = '';
-            foreach ($url_list as $u) {
-                $a_url_list .= $a_br
-                    . '<a href="' . htmlentities($u) . '" target="_BLANK">'
-                    . $u . '</a>';
-                $a_br = "<br/>\n";
-            }
-        }
-
-        $a_pw_list = '&nbsp;';
-        if (!empty($info[$i]["pridecredential"][0])) {
-            $pw_list = explode(' ', $info[$i]["pridecredential"][0]);
-            $a_br = '';
-            $a_pw_list = '';
-            foreach ($pw_list as $pw) {
-                $this_pat = '/^' . $CONF['key_prefix'] . '(.*)/';
-                if (!empty($CONF['key'])
-                    && preg_match($this_pat, $pw, $m))
-                {
-                    $this_epw = $m[1];
-                    $pw = macdir_decode($this_epw);
-                }
-                $a_pw_list .= $a_br . $pw;
-                $a_br = "<br/>\n";
-            }
-        }
-
-        echo "<tr>\n";
-        echo " <td>${a_maint_link}${a_desc}</td>\n";
-        echo " <td>${a_url_list}</td>\n";
-        echo ' <td>' . nbsp_html($info[$i]["linkuid"][0]) . "</td>\n";
-        echo " <td>${a_pw_list}</td>\n";
-        echo "</tr>\n";
-    }
-    echo "</tbody>\n";
-    echo "</table>\n";
+    display_links(1, 'Links', $info);
 } else {
     echo '<p class="error">No entries found.</p>' . "\n";
 }
+
+if (!empty($_SERVER['WEBAUTH_USER'])) {
+    $u           = $_SERVER['WEBAUTH_USER'];
+    $link_filter = "(|(prideReadUid=${u})(prideWriteUid=${u}))";
+    $filter      = '(&'
+                 . '(objectclass=pridelistobject)'
+                 . $base_filter
+                 . $link_filter
+                 . ')';
+    $sr = ldap_search($ds, $ldap_user_base, $filter, $return_attr);
+    ldap_sort($ds, $sr, 'description');
+    $info = ldap_get_entries($ds, $sr);
+    $ret_cnt = $info["count"];
+    if ($ret_cnt) {
+        display_links(0, 'Shared Links', $info);
+    }
+}
+
 ?>
 
 </div>
